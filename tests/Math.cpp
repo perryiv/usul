04fa6755970fc1e36f3f426516dd07f2142aadf3
Perry Miller <miller@ciespace.com>
2019-01-23 22:50:31 -0500

    Added test for subtracting two 3D vectors

--- tests/Math.cpp
+++ tests/Math.cpp
@@ -20,11 +20,11 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 //
-//  Helper function to test 3D vectors.
+//  Helper function to add 3D vectors.
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-template < class ScalarType > inline void testVec3()
+template < class ScalarType > inline void testAddVec3()
 {
   typedef typename Usul::Math::Vector3 < ScalarType > Vec3;
 
@@ -48,6 +48,34 @@ template < class ScalarType > inline void testVec3()
 
 ////////////////////////////////////////////////////////////////////////////////
 //
+//  Helper function to subtract 3D vectors.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+template < class ScalarType > inline void testSubtractVec3()
+{
+  typedef typename Usul::Math::Vector3 < ScalarType > Vec3;
+
+  Vec3 a ( 4, 5, 6 );
+  Vec3 b ( 1, 2, 3 );
+
+  Vec3 c;
+  Usul::Math::subtract ( a, b, c );
+
+  REQUIRE ( 3 == c[0] );
+  REQUIRE ( 3 == c[1] );
+  REQUIRE ( 3 == c[2] );
+
+  Vec3 d = Usul::Math::subtract ( a, b );
+
+  REQUIRE ( 3 == d[0] );
+  REQUIRE ( 3 == d[1] );
+  REQUIRE ( 3 == d[2] );
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
 //  Test the math functions.
 //
 ////////////////////////////////////////////////////////////////////////////////
@@ -58,19 +86,36 @@ TEST_CASE ( "Math functions" )
   {
     SECTION ( "Can add two 3D vectors" )
     {
-      testVec3 < char           > ();
-      testVec3 < short          > ();
-      testVec3 < int            > ();
-      testVec3 < long           > ();
+      testAddVec3 < char           > ();
+      testAddVec3 < short          > ();
+      testAddVec3 < int            > ();
+      testAddVec3 < long           > ();
 
-      testVec3 < unsigned char  > ();
-      testVec3 < unsigned short > ();
-      testVec3 < unsigned int   > ();
-      testVec3 < unsigned long  > ();
+      testAddVec3 < unsigned char  > ();
+      testAddVec3 < unsigned short > ();
+      testAddVec3 < unsigned int   > ();
+      testAddVec3 < unsigned long  > ();
 
-      testVec3 < float          > ();
-      testVec3 < double         > ();
-      testVec3 < long double    > ();
+      testAddVec3 < float          > ();
+      testAddVec3 < double         > ();
+      testAddVec3 < long double    > ();
+    }
+
+    SECTION ( "Can subtract two 3D vectors" )
+    {
+      testSubtractVec3 < char           > ();
+      testSubtractVec3 < short          > ();
+      testSubtractVec3 < int            > ();
+      testSubtractVec3 < long           > ();
+
+      testSubtractVec3 < unsigned char  > ();
+      testSubtractVec3 < unsigned short > ();
+      testSubtractVec3 < unsigned int   > ();
+      testSubtractVec3 < unsigned long  > ();
+
+      testSubtractVec3 < float          > ();
+      testSubtractVec3 < double         > ();
+      testSubtractVec3 < long double    > ();
     }
   }
 }
