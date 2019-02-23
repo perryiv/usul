3b506414067871f220536f5a41ffde127118be7a
Perry Miller <miller@ciespace.com>
2019-02-23 00:08:59 -0500

    Added more files and tests

---
+++ tests/Usul/Strings/Format.cpp
@@ -0,0 +1,48 @@
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2019, Perry L Miller IV
+//  All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Test the function(s) for strings.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Usul/Strings/Format.h"
+
+#include "catch2/catch.hpp"
+
+#include <string>
+
+
+const std::string expected = "\
+Quote 1: My name is Inigo Montoya. You killed my father. Prepare to die.\n\
+Quote 2: There’s something I ought to tell you. I’m not left-handed either.\n\
+Quote 3: No more rhymes now, I mean it.\n\
+Quote 4: Anybody want a peanut?";
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Test the cast function.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE ( "String formatting" )
+{
+  SECTION ( "Can format a string" )
+  {
+    const std::string answer = Usul::Strings::format (
+      "Quote ", 1, ": ", "My name is Inigo Montoya. You killed my father. Prepare to die.", '\n',
+      "Quote ", 2, ": ", "There’s something I ought to tell you. I’m not left-handed either.", '\n',
+      "Quote ", 3, ": ", "No more rhymes now, I mean it.", '\n',
+      "Quote ", 4, ": ", "Anybody want a peanut?"
+    );
+
+    REQUIRE ( answer == expected );
+  }
+}
