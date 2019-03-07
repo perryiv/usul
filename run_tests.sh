1d057820bac3429f2c93eb409af2a6d8749a81f2
Perry Miller <miller@ciespace.com>
2019-03-07 00:29:10 -0500

    -- Generating version string from major, minor, and patch numbers. -- Debug executable and library file names are now appended with the letter d. -- Using cmake abstraction for position independent code. -- Library file names include version string. -- Appropriate symlinks for library files are automatically created.

--- run_tests.sh
+++ run_tests.sh
@@ -13,7 +13,7 @@
 #
 ################################################################################
 
-# Exit immediately if one of the following commands do not return zero.
+# Exit immediately if one of the following commands does not return zero.
 set -e
 
 # Echo all the commands.
@@ -23,30 +23,28 @@ rm -rf build
 mkdir build
 cd build
 
-cmake ../ \
+mkdir release
+cd release
+
+cmake ../.. \
   -DCMAKE_VERBOSE_MAKEFILE=ON \
   -DCMAKE_BUILD_TYPE="Release" \
   -DUSUL_ENABLE_CODE_COVERAGE=OFF
 cmake --build .
 
-ls -al ./bin/usul_test
-file ./bin/usul_test
 ./bin/usul_test --abort --use-colour=yes --durations=no
 
-cd ../
-rm -rf build
-mkdir build
-cd build
+cd ..
+mkdir debug
+cd debug
 
-cmake ../ \
+cmake ../.. \
   -DCMAKE_VERBOSE_MAKEFILE=ON \
   -DCMAKE_BUILD_TYPE="Debug" \
   -DUSUL_ENABLE_CODE_COVERAGE=ON
 cmake --build .
 
-ls -al ./bin/usul_test
-file ./bin/usul_test
-./bin/usul_test --abort --use-colour=yes --durations=no
+./bin/usul_testd --abort --use-colour=yes --durations=no
 
-cd ../
+cd ../..
 gcovr
