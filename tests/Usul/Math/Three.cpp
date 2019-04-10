9344538d650d1d4e5a9fb9d96e83905147bd3dd8
Perry Miller <perry@modelspace.com>
2019-04-09 23:33:12 -0400

    Added real numbers to unProject test

--- tests/Usul/Math/Three.cpp
+++ tests/Usul/Math/Three.cpp
@@ -13,12 +13,15 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "Usul/Math/Three.h"
 #include "Usul/Math/Matrix44.h"
+#include "Usul/Math/Three.h"
 #include "Usul/Math/Vector3.h"
 #include "Usul/Math/Vector4.h"
+#include "Usul/Strings/Format.h"
 
 #include "catch2/catch.hpp"
+
+#include <cmath>
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -38,16 +41,27 @@ TEMPLATE_TEST_CASE ( "3D template math functions", "",
 
   SECTION ( "Un-projecting works" )
   {
-    const Vec3 screen ( 100, 100, 0 );
+    const Vec4 viewport ( 0, 0, 1600, 1058 );
+    const T x ( std::round ( viewport[2] * T ( 0.5 ) ) );
+    const T y ( std::round ( viewport[3] * T ( 0.5 ) ) );
+    const Vec3 screen ( x, y, 0 );
     const Matrix viewMatrix;
-    const Matrix projMatrix;
-    const Vec4 viewport ( 0, 0, 1000, 1000 );
-    Vec3 answer;
+    const Matrix projMatrix (
+      T ( 2.4678185965049204 ), T ( 0 ), T ( 0 ), T ( 0 ),
+      T ( 0 ), T ( 3.7320508075688776 ), T ( 0 ), T ( 0 ),
+      T ( 0 ), T ( 0 ), T ( -1.0002000200020005 ), T ( -0.06487231313253677 ),
+      T ( 0 ), T ( 0 ), T ( -1 ), T ( 0 )
+    );
 
-    // Not testing real numbers yet, just making sure the template arguments work.
+    Vec3 answer;
     const bool result = Usul::Math::unProject ( screen, viewMatrix, projMatrix, viewport, answer );
 
     REQUIRE ( true == result );
+
+    const std::string expect ( Usul::Strings::formatVector3 ( Vec3 ( 0, 0, T ( -0.03243291126899654 ) ) ) );
+    const std::string actual ( Usul::Strings::formatVector3 ( answer ) );
+
+    REQUIRE ( expect == actual );
   }
 
   SECTION ( "Making a line works" )
