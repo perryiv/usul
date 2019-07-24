897f966838940ea22a23e35f4e091fd411ace7bc
Andrew Cunningham <ACU@esi-group.com>
2019-07-24 09:46:23 -0700

    created conan.py for the project

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import python_requires
+import glob, os, subprocess
+
+intel = python_requires("intel_helper/0.0.0@vaone-dev/master")
+
+
+class Usul(intel.ConanFile):
+	name = "usul"
+	version = "0.1.0"
+	settings = "os", "arch", "compiler", "build_type"
+	options = {"shared":[True,False]}
+	default_options = {"shared":True}
+	no_copy_source = True
+	generators = ("cmake",)
+	scm = {"type": "git", "url": "auto"}
+
+ 
+
+ 
+	def build(self):
+		cmake = intel.CMake(self)
+		cmake.configure(
+			defs={
+				"CMAKE_BUILD_WITH_INSTALL_RPATH": True,
+				"CMAKE_DEBUG_POSTFIX": ""
+			}
+		)
+		cmake.build()
+
+	def package(self):
+		cmake = intel.CMake(self)
+		cmake.install()
+		
+	def package_info(self):
+		self.cpp_info.includedirs=["include"]
+		self.cpp_info.libdirs=["lib"]
+		self.cpp_info.libs=["usul"]
+		
+
