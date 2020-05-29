970adc7512f5c1c735e256b222d26fc34ef8c5cb
Perry Miller <perry@modelspace.com>
2020-05-29 17:09:14 -0400

    -- Added a header file with an inline function that returns a stack trace as a string. -- Using it introduces a dependency on the boost stacktrace library. -- No Usul source files use it.

---
+++ source/Usul/Errors/StackTrace.h
@@ -0,0 +1,52 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2019, Perry L Miller IV
+//  All rights reserved.
+//  MIT License: https://opensource.org/licenses/mit-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Return a stack trace as a string.
+//  If you include this header file you will need to link to Boost.Stacktrace.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _USUL_ERRORS_STACK_TRACE_H_
+#define _USUL_ERRORS_STACK_TRACE_H_
+
+#ifdef _WIN32
+# define NOMINMAX
+# define VC_EXTRALEAN
+# define WIN32_LEAN_AND_MEAN
+# include <windows.h>
+#endif
+
+#define BOOST_STACKTRACE_GNU_SOURCE_NOT_REQUIRED
+#include "boost/stacktrace.hpp"
+
+#include <sstream>
+#include <string>
+
+
+namespace Usul {
+namespace Errors {
+namespace StackTrace {
+
+
+inline std::string get()
+{
+  std::ostringstream out;
+  out << message << "\nstack:\n" << boost::stacktrace::stacktrace();
+  return out.str();
+};
+
+
+} // namespace StackTrace
+} // namespace Errors
+} // namespace Usul
+
+
+#endif // _USUL_ERRORS_STACK_TRACE_H_
