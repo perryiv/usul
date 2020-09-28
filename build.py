727bcbfe8ad5d6bb786fe6ec6f303394106a938a
Perry Miller <miller@ciespace.com>
2020-09-28 16:25:53 -0400

    Only building x86_64

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(archs=["x86_64"])
     builder.add_common_builds()
     builder.run()
