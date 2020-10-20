05fa3bed463aef010e3999c7befa135079a326b9
Perry Miller <miller@ciespace.com>
2020-10-19 23:57:27 -0400

    Azure pipelines test 52

--- scripts/configure_conan.sh
+++ scripts/configure_conan.sh
@@ -1,6 +1,6 @@
 set -e
 set -x
-sudo python3 -m pip install --upgrade pip
-sudo pip3 install conan --upgrade
-sudo pip3 install conan_package_tools
+python3 -m pip install --upgrade pip
+pip3 install conan --upgrade
+pip3 install conan_package_tools
 conan user
