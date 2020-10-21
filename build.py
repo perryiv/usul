e52715f1866eae56fae229503f13bb28f4fad128
Perry Miller <miller@ciespace.com>
2020-10-21 18:35:26 -0400

    Azure pipelines test 88

--- build.py
+++ build.py
@@ -3,8 +3,9 @@ import os
 
 if __name__ == "__main__":
 
-    if os.environ["CONAN_USE_DOCKER"] == "1"
+    if os.environ["CONAN_USE_DOCKER"] == "1":
         os.environ["CONAN_DOCKER_IMAGE"] = os.environ["THIS_JOB_DOCKER_IMAGE"]
+        print "Setting CONAN_DOCKER_IMAGE to ", os.environ["CONAN_DOCKER_IMAGE"]
 
     print "Operating system = ", os.uname().sysname
 
