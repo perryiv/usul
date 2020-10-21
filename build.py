02b9ee6a5f8560a0b62ac31712db0fb94ef4561e
Perry Miller <miller@ciespace.com>
2020-10-21 19:01:10 -0400

    Azure pipelines test 91

--- build.py
+++ build.py
@@ -4,11 +4,22 @@ import os
 
 if __name__ == "__main__":
 
-    if os.environ["CONAN_USE_DOCKER"] == "1":
+    if os.getenv ( "THIS_JOB_DOCKER_IMAGE" ) is not None:
         os.environ["CONAN_DOCKER_IMAGE"] = os.environ["THIS_JOB_DOCKER_IMAGE"]
-        print ( "Setting CONAN_DOCKER_IMAGE to ", os.environ["CONAN_DOCKER_IMAGE"] )
+        os.environ["CONAN_USE_DOCKER"] = True
+        print ( "Going to use docker" )
 
-    print ( "Operating system = ", sys.platform )
+    if sys.platform.startswith ( "linux" ):
+        os.environ["CONAN_GCC_VERSIONS"] = os.environ["THIS_JOB_COMPILER_VERSION"]
+        print ( "This is Linux" )
+
+    if sys.platform.startswith ( "darwin" ):
+        os.environ["CONAN_APPLE_CLANG_VERSIONS"] = os.environ["THIS_JOB_COMPILER_VERSION"]
+        print ( "This is Mac" )
+
+    if sys.platform.startswith ( "win32" ):
+        os.environ["CONAN_VISUAL_VERSIONS"] = os.environ["THIS_JOB_COMPILER_VERSION"]
+        print ( "This is Windows" )
 
     # builder = ConanMultiPackager(archs=["x86_64"])
     # builder.add_common_builds()
