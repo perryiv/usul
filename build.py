494d7da1573f2b245a7ac1caef35cf8ba6c45b08
Perry Miller <miller@ciespace.com>
2020-10-21 20:20:47 -0400

    Azure pipelines test 96

--- build.py
+++ build.py
@@ -1,28 +1,26 @@
-# from cpt.packager import ConanMultiPackager
+from cpt.packager import ConanMultiPackager
 import sys
 import os
 
 if __name__ == "__main__":
+
+    if sys.platform.startswith ( "linux" ):
+        os.environ["CONAN_GCC_VERSIONS"] = os.environ["THIS_JOB_COMPILER_VERSION"]
+        print ( "Building on Linux" )
+
+    if sys.platform.startswith ( "darwin" ):
+        os.environ["CONAN_APPLE_CLANG_VERSIONS"] = os.environ["THIS_JOB_COMPILER_VERSION"]
+        print ( "Building on Mac" )
+
+    if sys.platform.startswith ( "win32" ):
+        os.environ["CONAN_VISUAL_VERSIONS"] = os.environ["THIS_JOB_COMPILER_VERSION"]
+        print ( "Building on Windows" )
 
     if os.getenv ( "THIS_JOB_USE_DOCKER" ) == "1":
         os.environ["CONAN_DOCKER_IMAGE"] = os.environ["THIS_JOB_DOCKER_IMAGE"]
         os.environ["CONAN_USE_DOCKER"] = "1"
         print ( "Going to use docker" )
 
-    if sys.platform.startswith ( "linux" ):
-        os.environ["CONAN_GCC_VERSIONS"] = os.environ["THIS_JOB_COMPILER_VERSION"]
-        print ( "This is Linux" )
-
-    if sys.platform.startswith ( "darwin" ):
-        os.environ["CONAN_APPLE_CLANG_VERSIONS"] = os.environ["THIS_JOB_COMPILER_VERSION"]
-        print ( "This is Mac" )
-
-    if sys.platform.startswith ( "win32" ):
-        os.environ["CONAN_VISUAL_VERSIONS"] = os.environ["THIS_JOB_COMPILER_VERSION"]
-        print ( "This is Windows" )
-
-    print ( os.environ )
-
-    # builder = ConanMultiPackager(archs=["x86_64"])
-    # builder.add_common_builds()
-    # builder.run()
+    builder = ConanMultiPackager(archs=["x86_64"])
+    builder.add_common_builds()
+    builder.run()
