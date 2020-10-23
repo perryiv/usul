b35a45707b5cd18125cf44c9a22cc694c468abca
Perry Miller <miller@ciespace.com>
2020-10-23 13:43:57 -0400

    Small change to generate a rebuild of conan packages

--- build.py
+++ build.py
@@ -21,6 +21,8 @@ if __name__ == "__main__":
         os.environ["CONAN_USE_DOCKER"] = "1"
         print ( "Going to use docker" )
 
-    builder = ConanMultiPackager(archs=["x86_64"])
+    builder = ConanMultiPackager(
+        archs=["x86_64"],
+    )
     builder.add_common_builds()
     builder.run()
