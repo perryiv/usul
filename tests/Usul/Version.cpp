d1760c97827da8560a636adb3735c4d576fc511c
Perry Miller <perry@modelspace.com>
2020-11-29 00:03:18 -0500

    Incremented minor version

--- tests/Usul/Version.cpp
+++ tests/Usul/Version.cpp
@@ -29,7 +29,7 @@
 TEST_CASE ( "Version should be correct" )
 {
   REQUIRE ( Usul::Version::MAJOR == 3 );
-  REQUIRE ( Usul::Version::MINOR == 3 );
+  REQUIRE ( Usul::Version::MINOR == 4 );
   REQUIRE ( Usul::Version::PATCH == 3 );
-  REQUIRE ( std::string ( Usul::Version::STRING ) == std::string ( "3.3.3" ) );
+  REQUIRE ( std::string ( Usul::Version::STRING ) == std::string ( "3.4.3" ) );
 }
