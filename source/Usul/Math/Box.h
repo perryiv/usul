da3565744ff8808a974d725c3e75d7e554dcb1c9
Perry Miller <perry@modelspace.com>
2020-12-24 18:57:40 -0500

    Added box class and tests

---
+++ source/Usul/Math/Box.h
@@ -0,0 +1,212 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2020, Perry L Miller IV
+//  All rights reserved.
+//  MIT License: https://opensource.org/licenses/mit-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Class representing a bounding box.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _USUL_MATH_BOX_CLASS_H_
+#define _USUL_MATH_BOX_CLASS_H_
+
+#include "Usul/Math/Vector3.h"
+
+#include <limits>
+
+
+namespace Usul {
+namespace Math {
+
+
+template
+<
+  class T,
+  typename IndexType = unsigned int
+>
+class Box
+{
+public:
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Useful typedefs.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  typedef T value_type;
+  typedef Box < T, IndexType > ThisType;
+  typedef Usul::Math::Vector3 < T, IndexType > Vec3;
+  typedef std::numeric_limits < T > Limits;
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Constructors.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  constexpr Box() :
+    _min (  Limits::max(),  Limits::max(),  Limits::max() ), // These are correct.
+    _max ( -Limits::max(), -Limits::max(), -Limits::max() )
+  {
+  }
+  constexpr Box ( const Vec3 &mn, const Vec3 &mx ) :
+    _min ( mn ),
+    _max ( mx )
+  {
+  }
+  constexpr Box ( const Box &box ) :
+    _min ( box._min ),
+    _max ( box._max )
+  {
+  }
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Assignment.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  Box &operator = ( const Box &rhs )
+  {
+    _min = rhs._min;
+    _max = rhs._max;
+    return *this;
+  }
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Is the box valid?
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  bool valid() const
+  {
+    // We use <= because a point has a valid bounding box but zero size.
+    return ( ( _min[0] <= _max[0] ) && ( _min[1] <= _max[1] ) && ( _min[2] <= _max[2] ) );
+  }
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Get/set the min and max.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  const Vec3 &getMin() const
+  {
+    return _min;
+  }
+  void setMin ( const Vec3 &v )
+  {
+    _min = v;
+  }
+  const Vec3 &getMax() const
+  {
+    return _max;
+  }
+  void setMax ( const Vec3 &v )
+  {
+    _max = v;
+  }
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Grow the box.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  void grow ( const Vec3 &v )
+  {
+    this->_grow ( v, 0 );
+    this->_grow ( v, 1 );
+    this->_grow ( v, 2 );
+  }
+  void grow ( const ThisType &box )
+  {
+    this->grow ( box._min );
+    this->grow ( box._max );
+  }
+
+
+protected:
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Grow the box.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  void _grow ( const Vec3 &v, unsigned int index )
+  {
+    const value_type &value = v[index];
+
+    // Do both of these because if the box is invalid and it's grown by a
+    // single point, then that point is both the new min and max.
+    if ( value < _min[index] )
+    {
+      _min[index] = value;
+    }
+    if ( value > _max[index] )
+    {
+      _max[index] = value;
+    }
+  }
+
+
+private:
+
+  Vec3 _min;
+  Vec3 _max;
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  See if they are equal.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+template < class T, class I >
+inline bool equal ( const Box < T, I > &a, const Box < T, I > &b )
+{
+  return ( Usul::Math::equal ( a.getMin(), b.getMin() ) &&
+           Usul::Math::equal ( a.getMax(), b.getMax() ) );
+}
+template < class T, class I >
+inline bool operator == ( const Box < T, I > &a, const Box < T, I > &b )
+{
+  return ( true == Usul::Math::equal ( a, b ) );
+}
+template < class T, class I >
+inline bool operator != ( const Box < T, I > &a, const Box < T, I > &b )
+{
+  return ( false == Usul::Math::equal ( a, b ) );
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Useful typedefs.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+typedef Box < float  > Boxf;
+typedef Box < double > Boxd;
+
+
+} // namespace Math
+} // namespace Usul
+
+
+#endif // _USUL_MATH_BOX_CLASS_H_
---
+++ tests/Usul/Math/Box.cpp
@@ -0,0 +1,136 @@
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2020, Perry L Miller IV
+//  All rights reserved.
+//  MIT License: https://opensource.org/licenses/mit-license.html
+//
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Test the bounds class.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Usul/Math/Box.h"
+
+#include "catch2/catch.hpp"
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Test the bounds.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+TEMPLATE_TEST_CASE ( "Box class", "",
+  float,
+  double,
+  ( long double ) )
+{
+  typedef Usul::Math::Box < TestType > Box;
+  typedef typename Box::Vec3 Vec3;
+  typedef typename Box::Limits Limits;
+
+  constexpr Vec3 defaultMin (  Limits::max(),  Limits::max(),  Limits::max() );
+  constexpr Vec3 defaultMax ( -Limits::max(), -Limits::max(), -Limits::max() );
+
+  SECTION ( "Default constructor" )
+  {
+    const Box a;
+    REQUIRE ( false == a.valid() );
+    REQUIRE ( true == Usul::Math::equal ( defaultMin, a.getMin() ) );
+    REQUIRE ( true == Usul::Math::equal ( defaultMax, a.getMax() ) );
+  }
+
+  SECTION ( "Constructor from min and max" )
+  {
+    const Box a ( Vec3 ( 0, 0, 0 ), Vec3 ( 1, 1, 1 ) );
+    REQUIRE ( true == a.valid() );
+
+    const Box b ( Vec3 ( 1, 1, 1 ), Vec3 ( 0, 0, 0 ) );
+    REQUIRE ( false == b.valid() );
+  }
+
+  SECTION ( "Copy constructor" )
+  {
+    const Box a ( Vec3 ( 10, 10, 10 ), Vec3 ( 11, 11, 11 ) );
+    REQUIRE ( true == a.valid() );
+
+    const Box b ( a );
+    REQUIRE ( true == b.valid() );
+    REQUIRE ( true == Usul::Math::equal ( b, a ) );
+    REQUIRE ( b == a );
+
+    const Box c = a;
+    REQUIRE ( true == c.valid() );
+    REQUIRE ( true == Usul::Math::equal ( c, a ) );
+    REQUIRE ( c == a );
+  }
+
+  SECTION ( "Assignment" )
+  {
+    const Box a ( Vec3 ( 10, 10, 10 ), Vec3 ( 11, 11, 11 ) );
+    REQUIRE ( true == a.valid() );
+
+    Box b;
+    REQUIRE ( false == b.valid() );
+    REQUIRE ( true == Usul::Math::equal ( defaultMin, b.getMin() ) );
+    REQUIRE ( true == Usul::Math::equal ( defaultMax, b.getMax() ) );
+
+    b = a;
+    REQUIRE ( true == b.valid() );
+    REQUIRE ( true == Usul::Math::equal ( b, a ) );
+    REQUIRE ( b == a );
+  }
+
+  SECTION ( "Can grow the box" )
+  {
+    Box a;
+    REQUIRE ( false == a.valid() );
+
+    const Vec3 p0 ( 0, 0, 0 );
+    const Vec3 p1 ( 1, 1, 1 );
+    const Vec3 pn1 ( -1, -1, -1 );
+
+    a.grow ( p0 );
+    REQUIRE ( true == a.valid() );
+    REQUIRE ( p0[0] == a.getMin()[0] );
+    REQUIRE ( p0[2] == a.getMin()[2] );
+    REQUIRE ( p0[1] == a.getMin()[1] );
+    REQUIRE ( true == Usul::Math::equal ( p0, a.getMin() ) );
+    REQUIRE ( true == Usul::Math::equal ( p0, a.getMax() ) );
+
+    a.grow ( p1 );
+    REQUIRE ( true == a.valid() );
+    REQUIRE ( true == Usul::Math::equal ( p0, a.getMin() ) );
+    REQUIRE ( true == Usul::Math::equal ( p1, a.getMax() ) );
+
+    a.grow ( pn1 );
+    REQUIRE ( true == a.valid() );
+    REQUIRE ( true == Usul::Math::equal ( pn1, a.getMin() ) );
+    REQUIRE ( true == Usul::Math::equal ( p1,  a.getMax() ) );
+  }
+
+  SECTION ( "Can get the size" )
+  {
+    const Box a;
+    REQUIRE ( false == a.valid() );
+    REQUIRE ( true == Usul::Math::equal ( defaultSize, a.getSize() ) );
+
+    const Box b ( Vec3 ( -1, -1, -1 ), Vec3 ( 1, 1, 1 ) );
+    REQUIRE ( true == b.valid() );
+    REQUIRE ( true == Usul::Math::equal ( Vec3 ( 2, 2, 2 ), b.getSize() ) );
+  }
+
+  SECTION ( "Can get the center" )
+  {
+  }
+
+  SECTION ( "Can get the length of the diagonal" )
+  {
+    // const Vec3 size = b.getSize();
+    // REQUIRE ( true == Usul::Math::equal ( Vec3 ( std::sqrt ( ) ), b.getSize() ) );
+  }
+}
