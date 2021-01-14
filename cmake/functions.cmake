f1066f3ed70db55f689acc2901ffe8b777c746af
Perry Miller <perry@modelspace.com>
2021-01-13 19:45:07 -0500

    Better cmake output

---
+++ cmake/functions.cmake
@@ -0,0 +1,46 @@
+
+################################################################################
+#
+#  Copyright (c) 2021, Perry L Miller IV
+#  All rights reserved.
+#  MIT License: https://opensource.org/licenses/mit-license.html
+#
+################################################################################
+
+################################################################################
+#
+#  CMake functions.
+#
+################################################################################
+
+
+################################################################################
+#
+#  Function to print the package property.
+#
+################################################################################
+
+function(print_property)
+  if ( NOT ${${ARGV0}_${ARGV1}} STREQUAL "" )
+    message ( "   <> ${ARGV0}_${ARGV1} = ${${ARGV0}_${ARGV1}}" )
+  endif()
+endfunction()
+
+
+################################################################################
+#
+#  Function to print the package information.
+#
+################################################################################
+
+function(print_package)
+  print_property ( ${ARGV0} "FOUND" )
+  print_property ( ${ARGV0} "VERSION" )
+  print_property ( ${ARGV0} "INCLUDE_DIRS" )
+  print_property ( ${ARGV0} "INCLUDES" )
+  print_property ( ${ARGV0} "DEFINITIONS" )
+  print_property ( ${ARGV0} "LIBS" )
+  print_property ( ${ARGV0} "LIBRARIES" )
+  print_property ( ${ARGV0} "BUILD_MODULES" )
+  print_property ( ${ARGV0} "SYSTEM_LIBS" )
+endfunction()
