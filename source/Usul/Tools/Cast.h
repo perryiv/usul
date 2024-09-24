8130be3e549eeee064d3b071eabe03d62400b962
Perry Miller <Perry.Miller@esi-group.com>
2024-09-24 06:30:31 +0100

    Added canCastTo and safeCastTo functions

--- source/Usul/Tools/Cast.h
+++ source/Usul/Tools/Cast.h
@@ -9,18 +9,24 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  So that c-style casts are easy to find.
+//  Functions for casting.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef _USUL_TOOLS_UNSAFE_CAST_H_
-#define _USUL_TOOLS_UNSAFE_CAST_H_
+#pragma once
+
+#include <limits>
+#include <stdexcept>
+#include <type_traits>
 
 
 namespace Usul
 {
   namespace Tools
   {
+    //
+    //  So that c-style casts are easy to find.
+    //
     template < class ToType, class FromType >
     inline ToType unsafeCast ( FromType from )
     {
@@ -39,12 +45,61 @@ namespace Usul
       # pragma GCC diagnostic pop
       #endif
     }
+
+    //
+    //	Helper classes.
+    //
+    template < class ToType, class FromType > struct CanCast;
+    template <> struct CanCast < unsigned int, std::size_t >
+    {
+      typedef std::size_t FromType;
+      typedef unsigned int ToType;
+
+      typedef std::numeric_limits < ToType > ToTypeLimits;
+      typedef std::numeric_limits < FromType > FromTypeLimits;
+
+      static bool canCastTo ( FromType from )
+      {
+        static_assert ( std::is_integral_v < FromType > );
+        static_assert ( std::is_integral_v < ToType > );
+        static_assert ( std::is_unsigned_v < FromType > );
+        static_assert ( std::is_unsigned_v < ToType > );
+
+        // We're trying to go from a large unsigned type to a smaller one.
+        static_assert ( sizeof ( FromType ) >= sizeof ( ToType ) );
+
+        return ( from <= ( static_cast < FromType > ( ToTypeLimits::max() ) ) );
+      }
+    };
+
+    //
+    //	Make sure we can safely cast.
+    //
+    template < class ToType, class FromType >
+    inline bool canCastTo ( FromType from )
+    {
+      typedef CanCast < ToType, FromType > LocalType;
+      return LocalType::canCastTo ( from );
+    }
+
+    //
+    //	Safely cast. Throws an exception if there will be an overflow.
+    //
+    template < class ToType, class FromType >
+    inline bool safeCastTo ( FromType from )
+    {
+      if ( false == canCastTo < ToType > ( from ) )
+      {
+        throw std::invalid_argument ( "Invalid cast" );
+      }
+      return ( static_cast < ToType > ( from ) );
+    }
   }
 }
 
 
+//
+//  So that c-style casts are easy to find.
+//
 #define USUL_UNSAFE_CAST(to_type,from_object)\
   Usul::Tools::unsafeCast < to_type > ( from_object )
-
-
-#endif // _USUL_TOOLS_UNSAFE_CAST_H_
